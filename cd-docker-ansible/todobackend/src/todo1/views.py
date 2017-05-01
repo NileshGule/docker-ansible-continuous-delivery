6b9754e9c7aba84a1e43022f36369a0cd9bf1592
Nilesh Gule <vn_nilesh@yahoo.com>
2017-05-01 22:27:48 +0800

    added serializers and viewa with get and delete functions for todoitems

--- cd-docker-ansible/todobackend/src/todo1/views.py
+++ cd-docker-ansible/todobackend/src/todo1/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from todo1.models import TodoItem
+from todo1.serializers import TodoItemSerializer
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.reverse import reverse
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
 # Create your views here.
+class TodoItemViewSet(viewsets.ModelViewSet):
+    queryset = TodoItem.objects.all()
+    serializers.class  = TodoItemSerializer
+
+    def perform_create(self, serializer):
+        # Save instance to get the primary key and then update the url
+        instance = serializer.save()
+        instance.url = reverse('todoitem-detail', args=[instance.pk], request=self.request)
+        instance.save()
+
+        # Delete all todo items
+        def delete(self, request):
+            TodoItem.objects.all().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
